39ab153b2300bc4c49cd9f93b9c7677e802427f6
Krystian <Krystian@DESKTOP-O1QLI3L>
2023-07-17 19:22:36 +0200

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,44 @@
+﻿using LibraryAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+        public BooksController()
+        {
+
+        }
+
+
+        [HttpGet()]
+
+        public async Task<ActionResult<List<Book>>> GetAll()
+        {
+            var books = new List<Book>();
+            return Ok(books);
+        }
+
+        [HttpGet("/{id}")]
+        public async Task<ActionResult<Book>> Get([FromRoute] int id)
+        {
+            return Ok(new Book());
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace LibraryAPI.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+        public int NumberOfPages { get; set; }
+        public virtual Category Category { get; set; }
+        public int CategoryID { get; set; }
+
+
+    }
+}
